c6e31c5f1086b6d7dc7c87a326a5db2e78fbac10
tchiaran <tchiaran@BE19-14-L.sfasp2mi.univ-poitiers.fr>
2020-01-30 13:37:31 +0100

    Update index.html

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js')
 
 const client = new Discord.Client()
-console.log(process)
-console.log($TOKEN)
+console.log(process.env.TOKEN)
 client.login(process.env.TOKEN)
 client.on('message', msg => {
     const guildTag = msg.channel.type === 'text' ? `[${msg.guild.name}]` : '[DM]'
     const channelTag = msg.channel.type === 'text' ? `[#${msg.channel.name}]` : ''
     console.log(`${guildTag}${channelTag} ${msg.author.tag} : ${msg.content}`)
-});
+})
